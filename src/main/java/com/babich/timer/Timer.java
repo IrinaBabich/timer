73f9bdcfddf53df8d96c18bbf8926a79a412cb2e
Irina Babich <iringjob@gmail.com>
2018-06-29 01:50:01 +0300

    second commit

--- src/main/java/com/babich/timer/Timer.java
+++ src/main/java/com/babich/timer/Timer.java
@@ -1,4 +1,24 @@
 package com.babich.timer;
+// Написать класс Timer который оперирует двумя параметрами: имя и количество секунд до конца.
+// Каждую секунду выводит в консоль информацию о текущем состоянии таймера ->
+// имя + количество секунд до финиша.
+// По прошествии времени, выдает месседж о конце работы. Таймер должен работать асинхронно.
+// Протестировать приложение с 2-3 таймерами запущенными параллельно.
 
-public class Timer {
+public class Timer implements Runnable{
+    private String name;
+    private int timeToFinish;
+
+    @Override
+    public void run() {
+        while (timeToFinish > 0) {
+            System.out.println("Name: " + name + ", " + "time to finish: " + timeToFinish);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            timeToFinish--;
+        }
+    }
 }
